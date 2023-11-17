e4484aba164c7a819918b062a53e1ed13c6c2a74
Hugo Lemos <hugolemosedf2004@gmail.com>
2023-11-17 11:56:28 +0000

    fixed compileSDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.museumexplore"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.museumexplore"

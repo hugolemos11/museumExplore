25e22caa0152c7048a5e9d5a5491e0b1f739917b
Hugo Lemos <hugolemosedf2004@gmail.com>
2023-11-22 14:11:34 +0000

    fixed colors

--- app/src/main/java/com/example/museumexplore/MainActivity.kt
+++ app/src/main/java/com/example/museumexplore/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.register_page)
+        setContentView(R.layout.login_page)
     }
 }

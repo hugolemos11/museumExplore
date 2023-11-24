c1d2187c6803d2b74ff5ed016071b6b71cefe4a4
Hugo Lemos <hugolemosedf2004@gmail.com>
2023-11-24 17:23:36 +0000

    login has bugs

--- app/src/main/java/com/example/museumexplore/LoginActivity.kt
+++ app/src/main/java/com/example/museumexplore/LoginActivity.kt
@@ -1,7 +1,11 @@
 package com.example.museumexplore
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.museumexplore.databinding.LoginPageBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -11,5 +15,30 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = LoginPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginButton.setOnClickListener {
+            val email = binding.editTextEmailAddress.toString()
+            val password = binding.editTextPassword.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        val user = auth.currentUser
+                        val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+        }
     }
 }

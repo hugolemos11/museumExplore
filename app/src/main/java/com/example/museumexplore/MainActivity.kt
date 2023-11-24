26bcc896875b5168e963fe32e9b31eb8747e60ed
Hugo Lemos <hugolemosedf2004@gmail.com>
2023-11-24 15:43:36 +0000

    added register

--- app/src/main/java/com/example/museumexplore/MainActivity.kt
+++ app/src/main/java/com/example/museumexplore/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.museum_details)
+        setContentView(R.layout.register_page)
     }
 }

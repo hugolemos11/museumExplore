307460ec8ac4d101626a1418d7b9678ba03d14df
Hugo Lemos <hugolemosedf2004@gmail.com>
2023-11-24 17:16:09 +0000

    fixed register

--- app/src/main/java/com/example/museumexplore/LoginActivity.kt
+++ app/src/main/java/com/example/museumexplore/LoginActivity.kt
@@ -2,9 +2,14 @@ package com.example.museumexplore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.museumexplore.databinding.LoginPageBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: LoginPageBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = LoginPageBinding.inflate(layoutInflater)
     }
 }

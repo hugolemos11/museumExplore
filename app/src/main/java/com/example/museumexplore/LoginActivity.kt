89001a8dbc7a2352334955ce61aeb5165f51fb2e
Hugo Lemos <hugolemosedf2004@gmail.com>
2023-11-24 16:51:01 +0000

    trying to register

---
+++ app/src/main/java/com/example/museumexplore/LoginActivity.kt
@@ -0,0 +1,10 @@
+package com.example.museumexplore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/museumexplore/RegisterActivity.kt
@@ -0,0 +1,42 @@
+package com.example.museumexplore
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.museumexplore.databinding.RegisterPageBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: RegisterPageBinding
+    private lateinit var auth : FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = RegisterPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.registerButton.setOnClickListener {
+            val email = binding.editTextEmailAddress.toString()
+            val username = binding.editTextUsername.toString()
+            val password = binding.editTextPassword.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(
+                            baseContext,
+                            "Erro no registo",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+    }
+}

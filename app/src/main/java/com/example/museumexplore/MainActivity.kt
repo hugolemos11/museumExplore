672e37e3e3f5b9b19b24d5725b9cfa0ecb26607f
brunobacc <bacc121004@gmail.com>
2023-12-21 17:09:09 +0000

    Drawer e ActionBar funcionais

--- app/src/main/java/com/example/museumexplore/MainActivity.kt
+++ app/src/main/java/com/example/museumexplore/MainActivity.kt
@@ -8,34 +8,42 @@ import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.museumexplore.ui.home.HomeFragment
+import com.example.museumexplore.ui.home.MuseumDetailsFragment
 import com.google.android.material.navigation.NavigationView
 
 // , NavigationView.OnNavigationItemSelectedListener
 class MainActivity : AppCompatActivity(){
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.main_activity)
-
-
 
         drawerLayout = findViewById(R.id.drawer_layout)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
         val navigationView = findViewById<NavigationView>(R.id.nav_view)
 
         val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_nav, R.string.close_nav)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         toolbar.setNavigationOnClickListener {
             if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
@@ -50,22 +58,42 @@ class MainActivity : AppCompatActivity(){
             supportFragmentManager.beginTransaction()
                 .replace(R.id.nav_host_fragment, HomeFragment()).commit()
         }*/
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
+        // prevent nav gesture if not on start destination
+        navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, args: Bundle? ->
+            if (nd.id == nc.graph.startDestinationId /*|| nd.id == R.id.museumDetailsFragment*/) {
+                toolbar.setNavigationOnClickListener {
+                    if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+                        drawerLayout.closeDrawer(GravityCompat.END)
+                    } else {
+                        drawerLayout.openDrawer(GravityCompat.END)
+                    }
+                }
+            } else {
+                toolbar.setNavigationOnClickListener {
+                    onBackPressed()
+                }
+            }
+        }
 
         NavigationUI.setupWithNavController(navigationView, navController)
 
-        val appBarConfiguration = AppBarConfiguration(
+        appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.homeFragment,
-                R.id.museumDetailsFragment,
-               // R.id.navigation_settings // Add other destination IDs if needed
-            )
+                //R.id.museumDetailsFragment,
+               // R.id.navigation_settings // Add other destinatio  n IDs if needed
+            ),
+            drawerLayout,
+            fallbackOnNavigateUpListener = ::onSupportNavigateUp
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navigationView.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
 
    /* override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val menuToUse: Int = R.menu.right_menu
--- app/src/main/java/com/example/museumexplore/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/museumexplore/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
@@ -41,6 +42,9 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Remove the title of fragment on the actionBar
+        (activity as AppCompatActivity).supportActionBar?.title = ""
 
         navController = Navigation.findNavController(view);
 
